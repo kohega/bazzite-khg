aef9448e5f04d5136e9f06c5c8f39f13cb5df578
kohega <45211407+kohega@users.noreply.github.com>
2025-12-07 15:48:59 +0100

    try ai-fix howdy repo

--- build_files/install_packages.sh
+++ build_files/install_packages.sh
@@ -8,57 +8,28 @@ log() {
   echo "=== $* ==="
 }
 
-log "Install Howdy"
-
-git clone https://github.com/boltgolt/howdy
-cd howdy/
-
-dnf install -y \
-    git \
-    python3-devel \
-    cmake \
-    gcc-c++ \
-    pam-devel \
-    inih-devel \
-    opencv \
-    opencv-devel \
-    meson \
-    libevdev
-
-meson setup build
-meson compile -C build
-meson install -C build
-
-rm -rf ../howdy
-dnf remove -y \
-    cmake \
-    gcc-c++ \
-    meson
-
 log "Installing RPM packages"
-
-# Install RPMs
 for rpm_file in ctx/rpm/*.rpm; do
     if [ -f "$rpm_file" ]; then
         dnf5 install -y "$rpm_file"
     fi
 done
 
-log "Enable Copr repos"
+log "Enabling COPR repos"
 COPR_REPOS=(
     ilyaz/LACT
     zliced13/YACR
     atim/heroic-games-launcher
     zeno/scrcpy
     lnvso/heroic-games-launcher
-    principis/howdy-beta
+    #principis/howdy-beta
+    ronnypfannschmidt/howdy-beta
 )
 for repo in "${COPR_REPOS[@]}"; do
     dnf5 -y copr enable "$repo"
 done
 
 log "Install layered applications"
-# Layered Applications
 LAYERED_PACKAGES=(
     aria2c
     kcalc
@@ -88,13 +59,13 @@ LAYERED_PACKAGES=(
     gh
     qbittorrent
     discord
-    #python-elevate
-    #python-keyboard
-    #python-pyv4l2
+    python-elevate
+    python-keyboard
+    python-pyv4l2
     opencv
     opencv-devel
     v4l-utils
-    #howdy
+    howdy
     xorg-x11-font-utils
     merkuro
     kdepim-runtime
@@ -103,16 +74,15 @@ LAYERED_PACKAGES=(
 )
 dnf5 install --setopt=install_weak_deps=False --allowerasing --skip-unavailable --enable-repo="*rpmfusion*" -y "${LAYERED_PACKAGES[@]}"
 
-# Install Citrix
+log "Installing Citrix"
 /ctx/download-icaclient.sh
 rpm -i --nodeps ICAClient-rhel-*.rpm
 rm ICAClient-rhel-*.rpm
 
-# Microsoft fonts    
+log "Installing Microsoft fonts"    
 rpm -i --nodigest --nodeps https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
-log "Disable Copr repos as we do not need it anymore"
-
+log "Disable COPR repos as we do not need it anymore"
 for repo in "${COPR_REPOS[@]}"; do
     dnf5 -y copr disable "$repo"
 done
@@ -120,7 +90,6 @@ done
 log "Installing ZeroTier"
 # Add ZeroTier GPG key
 curl -s https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg | tee /etc/pki/rpm-gpg/RPM-GPG-KEY-zerotier
-
 # Add ZeroTier repository
 cat << 'EOF' | tee /etc/yum.repos.d/zerotier.repo
 [zerotier]
@@ -132,6 +101,5 @@ EOF
 
 # Install ZeroTier
 dnf install -y zerotier-one
-
 # Remove repos
 rm /etc/yum.repos.d/zerotier.repo -f
