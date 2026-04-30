b0418a76f5205cc4208bd574432bca8345e2db21
Kohega <kohega@tutanota.com>
2026-04-30 16:56:23 +0200

    rpmfusion fix

--- build_files/install_packages.sh
+++ build_files/install_packages.sh
@@ -34,7 +34,6 @@ dnf5 -y install \
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && \
 dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*" "*rpmfusion*".enabled=0 && \
-dnf5 -y install --enable-repo="*rpmfusion*" --disable-repo="*fedora-multimedia*" \
 
 log "Install layered applications"
 LAYERED_PACKAGES=(
@@ -79,7 +78,7 @@ LAYERED_PACKAGES=(
     lsp-plugins-lv2
     
 )
-dnf5 install --setopt=install_weak_deps=False --allowerasing --skip-unavailable --enable-repo="*rpmfusion*" -y "${LAYERED_PACKAGES[@]}"
+dnf5 install --setopt=install_weak_deps=False --allowerasing --skip-unavailable -y "${LAYERED_PACKAGES[@]}"
 
 log "Installing Citrix"
 /ctx/download-icaclient.sh
