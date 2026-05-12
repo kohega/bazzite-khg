4dd789a6b349561d4493e24c14777843dc1fc8f6
Kohega <kohega@tutanota.com>
2026-05-12 21:46:00 +0200

    undo

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -23,6 +23,5 @@ find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec
 mkdir -p /tmp
 mkdir -p /var/tmp \
 && chmod -R 1777 /var/tmp
-rm -rf /ctx
 
 log "Cleanup completed"
