c17d266ba604d636b5d72961287bbb8e4bea6474
kohega <45211407+kohega@users.noreply.github.com>
2026-06-08 15:36:43 +0200

    fix

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -12,24 +12,19 @@ log "Starting system cleanup"
 # Clean package manager cache
 dnf5 clean all
 
-# Clean temporary files
-rm -rf /tmp/*
+# Remove tmp files and everything in dirs that make bootc unhappy
+rm -rf /tmp/* || true
+rm -rf /usr/etc
+rm -rf /boot && mkdir /boot
 
 # (re)create essential system directories
 mkdir -p /sysroot /boot /usr/lib/ostree /var /tmp /var/tmp
 chmod -R 1777 /var/tmp
 
 # Clean /var directory while preserving essential files
-find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
-find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
+find /var/* -maxdepth 0 -type d \! -name cache \! -name log -exec rm -rf {} \;
+find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 -exec rm -rf {} \;
 
-# create symlinks for bootc filesystem layout
-ln -sT sysroot/ostree /ostree
-ln -sT var/roothome /root
-ln -sT var/srv /srv
-ln -sT var/mnt /mnt
-ln -sT var/opt /opt
-ln -sT var/home /home
-ln -sT var/usrlocal /usr/local
+ln -sfT var/usrlocal /usr/local
 
 log "Cleanup completed"
